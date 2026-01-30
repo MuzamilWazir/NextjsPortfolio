a891daabd5508450a4668f977f9740df9ab43321
MuzamilWazir <wazirmuhammadmuzammil@gmail.com>
2026-01-30 10:43:03 +0500

    profile pic upfate

--- components/sections/Home/Home.jsx
+++ components/sections/Home/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
         <div className="grid grid-cols-1 lg:grid-cols-6 gap-8 lg:gap-12 items-center lg:pt-20 pt-14">
           <Social />
           <div
-            className="relative w-64 h-64 md:w-72 md:h-72 lg:w-80 lg:h-80 rounded-[60%_40%_30%_70%/60%_30%_70%_40%] bg-cover bg-center bg-[url('/profile.png')] shadow-[inset_0_0_0_9px_rgba(173,173,170,0.26)] mx-auto lg:mx-0 lg:order-4 order-2"
+            className="relative w-64 h-64 md:w-72 md:h-72 lg:w-80 lg:h-80 rounded-[60%_40%_30%_70%/60%_30%_70%_40%] bg-cover bg-center bg-[url('/profile-pic.png')] shadow-[inset_0_0_0_9px_rgba(173,173,170,0.26)] mx-auto lg:mx-0 lg:order-4 order-2"
             style={{
               animation: "profile_animate 8s ease-in-out infinite 1s",
             }}
